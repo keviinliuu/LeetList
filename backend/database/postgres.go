81227b9951976c79f34e03d6b6e5b9178d1648d2
Kevin Liu <kevinliu676@yahoo.ca>
2024-01-16 23:54:33 -0500

    setting up

---
+++ backend/database/postgres.go
@@ -0,0 +1,32 @@
+package database
+
+import (
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Config struct {
+	Host		string
+	Port		string
+	Password	string
+	User 		string
+	DBName		string
+	SSLMode		string
+}
+
+func NewConnection(config *Config)(*gorm.DB, error) {
+	dsn := fmt.Sprintf(
+		"host=%s port=%s password=%s dbname=%s user=%s sslmode=%s", 
+		config.Host, config.Port, config.Password, config.User, config.DBName, config.SSLMode)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		return db, err
+	}
+
+	fmt.Printf("Successfully connected to database.")
+
+	return db, nil
+}
--- backend/server.go
+++ backend/server.go
@@ -5,18 +5,38 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/keviinliuu/leetlist/database"
 	"github.com/keviinliuu/leetlist/graph"
 )
 
 const defaultPort = "8080"
 
 func main() {
+	
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
+
+	dbConfig := &database.Config {
+		Host: os.Getenv("DB_HOST"),
+		Port: os.Getenv("DB_PORT"),
+		Password: os.Getenv("DB_PASS"),
+		User: os.Getenv("DB_USER"),
+		DBName: os.Getenv("DB_NAME"),
+		SSLMode: os.Getenv("DB_SSLMODE"),
+	}
+	
+	db, err := database.NewConnection(dbConfig)
+	
 
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
 
